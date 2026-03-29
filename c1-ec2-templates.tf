0936a15673edb2630845e3754af8cf8fa72adfda
Subbu <subramanien@gmail.com>
2026-03-29 07:12:53 +0000

    instance type modified

--- c1-ec2-templates.tf
+++ c1-ec2-templates.tf
@@ -1,4 +1,4 @@
-  resource "aws_launch_template" "ec2_instance" {
+/*  resource "aws_launch_template" "ec2_instance" {
   name_prefix   = "Subbu-web-launch-template"
   image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type["dev"]
@@ -11,6 +11,23 @@ yum install httpd -y
 systemctl start httpd
 systemctl enable httpd
 echo "Hello from Auto Scaling" > /var/www/html/index.html
+EOF
+)
+} */
+resource "aws_launch_template" "ec2_instance" {
+  name_prefix   = "gopal-web-launch-template"
+  image_id      = data.aws_ami.amazon_linux.id
+  instance_type = var.instance_type["dev"]
+  update_default_version = true #we are upding the default version
+
+  key_name               = "gopal"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+ user_data = base64encode(<<EOF
+#!/bin/bash
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+echo "Hello from Auto Scaling $HOSTNAME" > /var/www/html/index.html
 EOF
 )
 }
