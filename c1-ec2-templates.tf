009444c7e4e3ae9f825489ae4bc488202126edfb
Subbu <subramanien@gmail.com>
2026-03-29 08:41:32 +0000

    instance type modified template names

--- c1-ec2-templates.tf
+++ c1-ec2-templates.tf
@@ -21,7 +21,7 @@ resource "aws_launch_template" "ec2_instance" {
   instance_type = var.instance_type["dev"]
   update_default_version = true #we are upding the default version
 
-  key_name               = "subbu"
+  key_name               = "Subbu"
   vpc_security_group_ids = [aws_security_group.web_sg.id]
  user_data = base64encode(<<EOF
 #!/bin/bash
